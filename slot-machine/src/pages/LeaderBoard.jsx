7889fb8898952f23d48e6892d76559c50482bf55
marmelad444 <1111marmelad1111@gmail.com>
2026-01-19 06:14:58 +0000

    123

--- slot-machine/src/pages/LeaderBoard.jsx
+++ slot-machine/src/pages/LeaderBoard.jsx
@@ -35,7 +35,7 @@ const LeaderBoard = () => {
                             place={i + 1}
                             username={u.username}
                             escore={u.escore}
-                            isMe={u.id === user.id}
+                            isMe={u.id === user?.user?.userId}
                         />
                     ))}
                 </div>
